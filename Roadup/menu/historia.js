6d10a226213c56a8c2b39b6ddbcbc94ca947083d
Filipe da Silva Granja <110956203+filipegranja98@users.noreply.github.com>
2022-11-18 14:21:44 -0300

    criando arquivo js
    criei um arquivo js para colocar um script

---
+++ Roadup/menu/historia.js
@@ -0,0 +1,8 @@
+var resposta = window.prompt('Você também ama tecnologia?')
+       if(resposta.toLowerCase() == 'sim'){
+        window.alert('Você está no lugar certo!');
+       }else if(resposta.toLowerCase() == 'nao' | resposta.toLowerCase() == 'não'){
+        window.alert('Vai começar a gosta agora!')
+       }else{
+        window.alert('Obrigado por acessa nosso site!')
+       }
